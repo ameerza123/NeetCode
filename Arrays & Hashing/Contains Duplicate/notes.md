ba9ad428105cbe250e20c2bdc021aa2beb78025e
ameerza <ameerza@outlook.com>
2025-07-23 19:07:13 +1000

    Improved comments

--- Arrays & Hashing/Contains Duplicate/notes.md
+++ Arrays & Hashing/Contains Duplicate/notes.md
@@ -1,5 +1,15 @@
 # Solution 1
 
-Variable storing current item in list
-Loop through list comparing current variable to all other items in list
-If same value, return true
+Initial thoughts:
+- Variable storing current item in list
+- Loop through list comparing current variable to all other items in list
+- If same value, return true
+
+Time Complexity: O(n^2) - two nested loops iterating over array of size n
+Space Complexity: O(1) - no extra data structures
+
+Solution did work, but has poor time and space complexity as it is a brute-force method.
+Reading the question hints suggests that a better solution would require the use of a hashmap.
+
+# Solution 2
+
--- Arrays & Hashing/Contains Duplicate/solution1.java
+++ Arrays & Hashing/Contains Duplicate/solution1.java
@@ -1,20 +1,26 @@
-public class solution1 {
+// Brute Force Solution
+
+class Solution1 {
     public boolean hasDuplicate(int[] nums) {
-        if (nums.length <= 1) {
+        if (nums.length <= 1) { // Checks if array is too small to contain duplicates, so return false
             return false;
         }
         
-        int foo = nums[0];
-        int bar = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            foo = nums[i];
-            for (int j = 0; j < nums.length; j++) {
-                bar = nums[j];
-                if (foo == bar && i != j) {
+        int foo = nums[0]; // Pointer 1
+        int bar = nums[0]; // Pointer 2
+
+        for (int i = 0; i < nums.length; i++) { // Loops through all of array
+            foo = nums[i]; // Pointer 1 set to first int in array
+
+            for (int j = i+1; j < nums.length; j++) { // Loops through array again
+                bar = nums[j]; // Pointer 2 set to int after pointer 1
+
+                if (foo == bar) { // Checks if values of pointers match (duplicate found)
                     return true;
                 }
             }
         }
-        return false;
+
+        return false; // No duplicates found
     }
 }
